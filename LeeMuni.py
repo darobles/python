9a14e80e4ef1949d7633c27c4cb0767588413e5e
Daniel Robles <bajozero.tm@gmail.com>
2019-06-19 12:48:21 -0400

    test2

--- LeeMuni.py
+++ LeeMuni.py
@@ -1,11 +1,14 @@
 from conexion.conexionbd import *
+from patron.comuna import Comuna
 
 def consultaParSis():
+	listaComunas = []
 	conn = conexion();
 	c = conn.cursor()
 	c.execute(u'select PAR_COD,PAR_VAL from TBL_PAR_SIS')
 	for row in c:
 	    print (row[0], "-", row[1])
 	conn.close()
+	return listaComunas
 
 consultaParSis()
